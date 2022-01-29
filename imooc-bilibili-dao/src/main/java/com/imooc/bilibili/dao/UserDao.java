e51593ba2534c9924a537ab75a8ebe1da7dd3698
Derail <13336146405@163.com>
2022-01-30 01:23:12 +0800

    Initial commit

---
+++ imooc-bilibili-dao/src/main/java/com/imooc/bilibili/dao/UserDao.java
@@ -0,0 +1,37 @@
+package com.imooc.bilibili.dao;
+
+import com.alibaba.fastjson.JSONObject;
+import com.imooc.bilibili.domain.User;
+import com.imooc.bilibili.domain.UserInfo;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Mapper
+public interface UserDao {
+
+    User getUserByPhone(String phone);
+
+    Integer addUser(User user);
+
+    Integer addUserInfo(UserInfo userInfo);
+
+    User getUserById(Long id);
+
+    UserInfo getUserInfoByUserId(Long userId);
+
+    Integer updateUsers(User user);
+
+    Integer updateUserInfos(UserInfo userInfo);
+
+    User getUserByPhoneOrEmail(@Param("phone") String phone, @Param("email") String email);
+
+    List<UserInfo> getUserInfoByUserIds(Set<Long> userIdList);
+
+    Integer pageCountUserInfos(Map<String, Object> params);
+
+    List<UserInfo> pageListUserInfos(JSONObject params);
+}
